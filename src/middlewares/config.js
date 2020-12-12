fcb9c919d1577a377a99af5199e2bd7842931bbb
jsong89 <45890241+juuuuuuun@users.noreply.github.com>
2020-12-11 23:44:06 -0500

    removed unused one

--- src/middlewares/config.js
+++ src/middlewares/config.js
@@ -4,13 +4,6 @@ const logger = require('../utils/logger');
 
 const result = dotenv.config();
 if (result.error && logger) {
-  logger.warn(
-    '\n\n\t💡 It appears that you have not yet configured a .env file.' +
-      '\n\t   Please refer to our documentation regarding environment configuration:' +
-      '\n\t   https://github.com/Seneca-CDOT/telescope/blob/master/docs/CONTRIBUTING.md\n'
-  );
-
-  // Try and use the example env instead if we're not in production
   if (process.env.NODE_ENV !== 'production') {
     const envSamplePath = path.resolve(process.cwd(), 'env.example');
     logger.log(`Attempting to use default env in ${envSamplePath} instead.\n`);
